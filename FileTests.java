7aae57d0add9c0caa5ea7e96d2dba5d936ac5a2c
mannatgill <mannatgill511@gmail.com>
2022-10-13 23:25:26 -0700

    added FileTests.java in addition to a folder and files to test out

---
+++ FileTests.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import javax.security.auth.kerberos.DelegationPermission;
+import org.junit.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.*; 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileTests {
+
+    public void testFiles(){
+        List<File> listOfFiles = new ArrayList<>(); 
+
+    }
+    
+}
