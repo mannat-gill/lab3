2b87d0f3fb0d97c74a5c475e6ec9bb73350a0e99
mannatgill <mannatgill511@gmail.com>
2022-10-13 17:31:17 -0700

    Added Test and fixed bugs in ArrayExamples.class

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -2,35 +2,50 @@
 
 public class ArrayExamples {
 
-  // Changes the input array to be in reversed order
+ 
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
+    for(int i = 0; i < arr.length/2; i += 1) {
+      int temp = arr[i]; 
       arr[i] = arr[arr.length - i - 1];
+      arr[arr.length - i - 1] = temp; 
     }
+    
+  
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
   // order
+  /* 
+     was originally setting the null array to the passed array,
+     rather than the other way around. and returning the wrong array
+     */
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+       newArray[arr.length - i- 1] = arr[i];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
   // lowest number when calculating. Returns 0 if there are no elements or just
   // 1 element in the array
+  //Issue: if duplicates of lowest number present, deletes all of them, instead of just one
   static double averageWithoutLowest(double[] arr) {
     if(arr.length < 2) { return 0.0; }
     double lowest = arr[0];
-    for(double num: arr) {
-      if(num < lowest) { lowest = num; }
+    int indexLow = 0; 
+    for(int i=0; i<arr.length; i++) {
+      if(arr[i] < lowest) { 
+        lowest = arr[i];
+        indexLow=i; 
+      }
     }
     double sum = 0;
-    for(double num: arr) {
-      if(num != lowest) { sum += num; }
+    for(int i=0; i<arr.length; i++){
+      if(i!=indexLow){
+        sum+=arr[i]; 
+      }
     }
     return sum / (arr.length - 1);
   }
--- ArrayTests.java
+++ ArrayTests.java
@@ -1,18 +1,45 @@
 import static org.junit.Assert.*;
+
+import javax.security.auth.kerberos.DelegationPermission;
+
 import org.junit.*;
 
 public class ArrayTests {
+
+  //ReverseInPlace 
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
+  @Test 
+  public void testReverseInPlaceWithValues(){
+    int[] input = {5,6,7,8};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{8,7,6,5}, input);
+  }
 
-
+ 
+  //Reverse Methods
+  //checking for empty arrays
   @Test
-  public void testReversed() {
+  public void test1Reversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  //checking for arrays with vals
+  @Test
+  public void testReversed() {
+    int[] input1 = {5,6,7};
+    assertArrayEquals(new int[]{7,6,5}, ArrayExamples.reversed(input1));
+  }
+
+  @Test 
+  public void testAvgWithoutLowest(){
+    double[] input = {4.0,1.0,1.0,1.0}; 
+    assertEquals(2, ArrayExamples.averageWithoutLowest(input), 0.1);
+  }
+
 }
